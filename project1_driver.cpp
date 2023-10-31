8976f3a1430983c6da12c7c5429d9056178f516e
George Davis <davisgl055@ecc.edu>
2023-10-31 02:18:42 -0400

    Think it works need to do more testing on edge cases, and make up good documentation

--- project1_driver.cpp
+++ project1_driver.cpp
@@ -8,12 +8,15 @@
 ** 
 ** Date Created: 10/29/2023
 ** 
-** Date Last Modified: 10/29/23 
+** Date Last Modified: 10/31/23 
 ** 
 ** 
 */
 #include<stdio.h>
 #include<iostream>
+#include<iomanip>
+#define checkIfYes choice == "y" || choice == "Y" || choice == "yes" || choice == "Yes" || choice == "YEs" || choice == "YES" || choice == "yES" || choice == "yEs" || choice == "yeS" || choice == "YeS"
+#define checkIfNo choice == "n" || choice == "N" || choice == "no" || choice == "No" || choice == "nO" || choice == "NO" 
 
 using namespace std;
 string getSocialSecurityNumber();
@@ -24,6 +27,7 @@ bool checkUserChoice();
 
 int main() 
 {
+	cout << fixed << setprecision(2) << showpoint;
 	bool still = true;
 	while (still)
 	{
@@ -37,41 +41,76 @@ int main()
 }
 
 
-string getSocialSecurityNumber() 
+int getIncome() 
 {
+	int income = 0;
 	while (true)
 	{
+		cout << "Please enter income: " << endl << endl;
+		cin >> income;
+		if (!cin)
+		{
+			cerr << "Invalid input please enter integer income " << endl << endl;
+			cin.clear();
+			cin.ignore(1e7, '\n');
+			income = 0;
+			continue;
+		}
+		else
+		{
+			cin.ignore(1e7, '\n'); // Clear buffer for next input and output operations
+			break;
+		}
+	}
+
+	return income;
+}
+
+string getSocialSecurityNumber() 
+{
+	string ssn; 
+	while (true)
+	{
+		cout << "Please input Social Security Number: ";
 		bool validSsn = true;
-		string ssn;
 		cin >> ssn;
 		for (int i = 0; i < ssn.size(); i++) 
 		{
-			if (!(ssn.at(i) <= '0' && ssn.at(i) >= '9'))
+			char index = ssn.at(i);
+			if (index <= '9' && index >= '0')
+			{
+				validSsn = true;
+				break;
+			}
+			else 
 			{
 				validSsn = false;
 				break;
 			}
 		}
-		if (!cin || ssn.size() != 8 || validSsn == false) 
+		if (!cin || ssn.size() != 9 || validSsn == false)
 		{
 			cerr << "Invalid input please input an integer Social Security Number 8 characters long in the form xxxxxxxxx." << endl << endl;
 			cin.ignore(1000000, '\n');
 		}
+		else
+			break;
 	}
+	return ssn;
 }
 
 bool checkUserChoice() 
 {
 	while (true)
 	{
-		cout << "Please input if you want to still continue y/n" << endl << endl;
+		cout << "\n\nPlease input if you want to still continue y/n" << endl << endl;
 		string choice;
 		cin >> choice;
-		if (choice == "y" || choice == "yes" || choice == "Y")
+		if (checkIfYes)
 		{
 			return true;
 		}
-		else if (choice == "n" || choice == "no" || choice == "N")
+		else if (checkIfNo)
 		{
 			break;
 		}
@@ -90,14 +129,16 @@ string strToLower()
 }
 
 
-double evaluateFederalIncomeTax(string socialSecurityNumber, int income) 
+double evaluateFederalIncomeTax(int income) 
 {
 	double tax = 0;
 	if (income > 0 && income < 24'650)
+	{
 		tax += (income * .15);
+	}
 	else if (income > 24'650 && income < 59'750)
 	{
-		tax += 3'697.5 + ((income -= 24'650) * .28);
+		tax += 3'697.5 + (income * .28);
 	}
 
 	else if (income > 59'750 && income < 124'650)
@@ -120,8 +161,7 @@ double evaluateFederalIncomeTax(string socialSecurityNumber, int income)
 
 
 
-void showFederalIncomeTax(string socialSecurityNumber, int income) 
+void showFederalIncomeTax(string socialSecurityNumber, double tax) 
 {
-	cout << "Hello " << socialSecurityNumber << "here is your federal income tax ";
-
+	cout << "Hello " << socialSecurityNumber << " here is your federal income tax  " << tax;
 }
